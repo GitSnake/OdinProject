2d8ea9f727d3f7b5f33a2f6ec16e8b245bb18ec4
gitSnake <mastermage1@gmail.com>
2018-09-05 05:27:08 -0400

    Added choices variables

--- 01-web-dev-101/04-javascript-basics/01b-rock-paper-scissors/script.js
+++ 01-web-dev-101/04-javascript-basics/01b-rock-paper-scissors/script.js
@@ -3,7 +3,10 @@ const computerScore = 0;
 const userScore_span = document.getElementById("userScore");
 const computerScore_span = document.getElementById("computerScore");
 const scoreBoard_div = document.querySelector(".score-board");
-
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
 
 
 // ======================================================== //
