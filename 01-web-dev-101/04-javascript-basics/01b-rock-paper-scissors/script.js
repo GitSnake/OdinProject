209c2f64385efaa9ccef7dca96c8c58191f043fa
gitSnake <mastermage1@gmail.com>
2018-09-05 08:33:38 -0400

    Math.random method

--- 01-web-dev-101/04-javascript-basics/01b-rock-paper-scissors/script.js
+++ 01-web-dev-101/04-javascript-basics/01b-rock-paper-scissors/script.js
@@ -10,7 +10,7 @@ const scissors_div = document.getElementById("s");
 
 function getComputerChoice(){
 	const choices = ['r', 'p', 's'];
-	
+	console.log(Math.random());
 }
 
 function game(userChoice){
