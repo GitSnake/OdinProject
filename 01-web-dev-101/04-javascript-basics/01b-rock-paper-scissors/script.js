07aeced0ee250c4cb725baa5a12ed99fecc22107
gitSnake <mastermage1@gmail.com>
2018-09-05 07:27:31 -0400

    rock test function

--- 01-web-dev-101/04-javascript-basics/01b-rock-paper-scissors/script.js
+++ 01-web-dev-101/04-javascript-basics/01b-rock-paper-scissors/script.js
@@ -8,6 +8,47 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+rock_div.addEventListener('click', function() {
+	console.log("hey you clicked on rock");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // ======================================================== //
 // userChoice is declared
