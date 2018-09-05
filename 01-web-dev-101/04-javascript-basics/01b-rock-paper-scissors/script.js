e4d6517c263493fc2ef43d5736a1c02ac6292b44
gitSnake <mastermage1@gmail.com>
2018-09-04 23:32:17 -0400

    Added a few switch cases and arrow functions and a for loop

--- 01-web-dev-101/04-javascript-basics/01b-rock-paper-scissors/script.js
+++ 01-web-dev-101/04-javascript-basics/01b-rock-paper-scissors/script.js
@@ -1,7 +1,19 @@
 // create function computerPlay (randomly return ‘Rock’, ‘Paper’ or ‘Scissors’)
 const multiplyES6 = (x, y) => { return x * y };
 
+switch(x) {
+  case 'value1':  // if (x === 'value1')
+    ...
+    [break]
 
+  case 'value2':  // if (x === 'value2')
+    ...
+    [break]
+
+  default:
+    ...
+    [break]
+}
 
 
 
@@ -9,6 +21,19 @@ const multiplyES6 = (x, y) => { return x * y };
 // funtion has two parameter (playerSelection, computerSelection)
 const RockGame = (playerSelection, computerSelection) => { return x * y };
 
+switch(x) {
+  case 'value1':  // if (x === 'value1')
+    ...
+    [break]
+
+  case 'value2':  // if (x === 'value2')
+    ...
+    [break]
+
+  default:
+    ...
+    [break]
+}
 
 // RockGame function returns a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 
@@ -24,8 +49,22 @@ const computerSelection = computerPlay()
 console.log(playRound(playerSelection, computerSelection))
 ​
 
-4. Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
+Write a NEW function called game(). 
+const game = (playerSelection, computerSelection) => { return x * y };
+
+Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
     1. At this point you should still just be using console.log() to display the results of each round and the winner at the end.
     2. Use prompt() to get input from the user. Read the docs here if you need to.
-    3. Feel free to re-work your previous functions if you need to. Specifically, you might want to change the return value to something more useful.
-    4. Feel free to create more “helper” functions if you think it would be useful.
+    3. Feel free to create more “helper” functions if you think it would be useful.
+
+
+
+for loop
+for (var i = Things.length - 1; i >= 0; i--) {
+	Things[i]
+}
+
+
+
+
+
