ef24d8c04fd6969cdd583b68ccfed0baae504940
gitSnake <mastermage1@gmail.com>
2018-09-05 05:17:57 -0400

    Added another variable

--- 01-web-dev-101/04-javascript-basics/01b-rock-paper-scissors/script.js
+++ 01-web-dev-101/04-javascript-basics/01b-rock-paper-scissors/script.js
@@ -2,7 +2,7 @@ const userScore = 0;
 const computerScore = 0;
 const userScore_span = document.getElementById("userScore");
 const computerScore_span = document.getElementById("computerScore");
-
+const scoreBoard_div = document.querySelector(".score-board");
 
 
 
