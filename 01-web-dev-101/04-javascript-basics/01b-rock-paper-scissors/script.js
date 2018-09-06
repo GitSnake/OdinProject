b5c082f399ac6199a1892c0e65b99456a6fef52a
gitSnake <mastermage1@gmail.com>
2018-09-06 06:17:31 -0400

    duplicate function codes for lose and tie

--- 01-web-dev-101/04-javascript-basics/01b-rock-paper-scissors/script.js
+++ 01-web-dev-101/04-javascript-basics/01b-rock-paper-scissors/script.js
@@ -21,20 +21,27 @@ function convertToWord(letter) {
 }
 
 function win(userChoice, computerChoice) {
-	userScore++;
 	userScore_span.innerHTML = userScore;
 	computerScore_span.innerHTML = computerScore;
-	const smallUserWord = "user".fontsize(3);
-	const smallCompWord = "comp".fontsize(3);
+	const smallUserWord = "user".fontsize(3).sub();
+	const smallCompWord = "comp".fontsize(3).sub();
 	result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}. You win!`;
 }
 
-function lose() {
-	console.log("Lose");
+function lose(userChoice, computerChoice) {
+	userScore_span.innerHTML = userScore;
+	computerScore_span.innerHTML = computerScore;
+	const smallUserWord = "user".fontsize(3).sub();
+	const smallCompWord = "comp".fontsize(3).sub();
+	result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}. You win!`;
 }
 
-function draw() {
-	console.log("Draw");
+function draw(userChoice, computerChoice) {
+	userScore_span.innerHTML = userScore;
+	computerScore_span.innerHTML = computerScore;
+	const smallUserWord = "user".fontsize(3).sub();
+	const smallCompWord = "comp".fontsize(3).sub();
+	result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}. You win!`;
 }
 
 function game(userChoice){
